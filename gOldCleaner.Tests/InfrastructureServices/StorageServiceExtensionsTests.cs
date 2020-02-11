98fd6450a1aa128767c16a113aa63137ff781e76
vdasus <me@vdasus.com>
2020-02-11 21:01:13 +0200

    StorageServiceTests and StorageServiceExtensionsTests

---
+++ gOldCleaner.Tests/InfrastructureServices/StorageServiceExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using gOldCleaner.InfrastructureServices;
+using System;
+using System.IO;
+using Xunit;
+
+namespace gOldCleaner.Tests.InfrastructureServices
+{
+    [Trait("Common", "Unit Test")]
+    public class StorageServiceExtensionsTests
+    {
+        [Fact]
+        //TODO Bad test, try to solve with abstract fs permissions
+        public void ExceptionsNotThrown()
+        {
+            var files = Directory.EnumerateFiles(@"C:\Windows\System32", "*.*", SearchOption.AllDirectories).SkipExceptions();
+
+            Action sut = () => { foreach (var file in files) { } };
+
+            sut.Should().NotThrow();
+        }
+    }
+}

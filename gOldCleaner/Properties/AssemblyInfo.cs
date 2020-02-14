8851aa00553aeea47f384d8f41534e3f966e7586
vdasus <me@vdasus.com>
2020-02-14 22:35:48 +0200

    moved to core

--- gOldCleaner/Properties/AssemblyInfo.cs
+++ gOldCleaner/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("gOldCleaner")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © vdasus 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 

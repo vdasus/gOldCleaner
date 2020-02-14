f75f4416e0bedcb771d7a4ce6ed89203f4f9e95e
vdasus <me@vdasus.com>
2020-02-15 00:33:02 +0200

    refactoring

--- gOldCleaner/DomainServices/FolderItemService.cs
+++ gOldCleaner/DomainServices/FolderItemService.cs
@@ -19,61 +19,77 @@ namespace gOldCleaner.DomainServices
         {
             _storage = storage;
             _informer = informer;
+            Result.ErrorMessagesSeparator = "\n";
         }
 
         public Result Cleanup(FolderItem folder)
         {
-            _informer?.LogDebug($"Processing {folder.Description} : {folder.FolderPath}...");
-
-            Result.ErrorMessagesSeparator = "\n";
+            _informer?.LogDebug($"Processing [{folder.Description}] : {folder.FolderPath}...");
 
             var dateDeleteAfter = DateTime.UtcNow - folder.DeleteAfter;
-            var result = Result.Ok();
+            var result = Result.Success();
 
-            var searchPattern = folder.SearchPattern.Split(',');
+            var searchPatterns = folder.SearchPattern.Split(',');
 
-            foreach (var oneSearchPattern in searchPattern)
-            {
-                try
-                {
-                    var files = _storage.EnumerateFiles(folder.FolderPath, oneSearchPattern,
-                        SearchOption.AllDirectories);
-                    foreach (var file in files)
-                    {
-                        if (_storage.GetLastWriteTimeUtc(file) <= dateDeleteAfter)
-                        {
-                            var isok = _storage.DeleteFile(file);
-                            result = Result.Combine(result, isok);
-                            _informer?.Inform(isok.IsSuccess ? "-" : "E");
-                            _informer?.LogDebug(isok.IsSuccess ? $"deleted {file}" : isok.Error);
+            result = searchPatterns.Aggregate(result,
+                (current, searchPattern) => 
+                    Result.Combine(current, CleanFolderBySearchPattern(folder, searchPattern, dateDeleteAfter)));
 
-                        }
-                        else
-                        {
-                            _informer?.Inform(".");
-                            _informer?.LogTrace($"skipped {file}");
-                        }
- }
-                }
-                catch(Exception ex)
-                {
-                    _informer?.Inform("E");
-                    _informer?.LogError(ex.Message);
-                }
-            }
+            _informer?.LogDebug($"Done [{folder.Description}] : {folder.FolderPath}");
 
-            _informer?.LogDebug($"Done {folder.Description} : {folder.FolderPath}");
             return result;
         }
 
         public Result DeleteEmptyFolders(FolderItem folder)
         {
-            var result = Result.Ok();
-            if (folder.IsDeleteEmptyFolders)
+            var result = Result.Success();
+            if (!folder.IsDeleteEmptyFolders) return result;
+
+            result = _storage.CleanEmptyFolders(folder.FolderPath);
+
+            _informer?.Inform("D");
+            _informer?.LogDebug($"folder [{folder.Description}] : {folder.FolderPath} cleaned up from empty folders");
+
+            return result;
+        }
+
+        private Result CleanFolderBySearchPattern(FolderItem folder, string oneSearchPattern, DateTime dateDeleteAfter)
+        {
+            var result = Result.Success();
+
+            try
             {
-                result = Result.Combine(result, _storage.CleanEmptyFolders(folder.FolderPath));
-                _informer?.Inform("D");
-                _informer?.LogDebug($"folder {folder.Description} : {folder.FolderPath} cleaned up");
+                var files = _storage.EnumerateFiles(folder.FolderPath, oneSearchPattern,
+                    SearchOption.AllDirectories);
+
+                result = files.Aggregate(result,
+                    (current, file) => Result.Combine(current, CleanOneFile(dateDeleteAfter, file)));
+            }
+            catch (Exception ex)
+            {
+                _informer?.Inform("E");
+                _informer?.LogError(ex.Message);
+                return Result.Failure(result.Error);
+            }
+
+            return result;
+        }
+
+        private Result CleanOneFile(DateTime dateDeleteAfter, string file)
+        {
+            var result = Result.Success();
+
+            var lastwritedate = _storage.GetLastWriteTimeUtc(file);
+            if (lastwritedate <= dateDeleteAfter)
+            {
+                result = _storage.DeleteFile(file);
+                _informer?.Inform(result.IsSuccess ? "-" : "E");
+                _informer?.LogDebug(result.IsSuccess ? $"deleted {file}" : $"{result.Error} (LW:{lastwritedate})");
+            }
+            else
+            {
+                _informer?.Inform(".");
+                _informer?.LogTrace($"skipped {file}");
             }
 
             return result;
